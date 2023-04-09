d9fbaabf035e5048e5d11194542acf9784252d3e
Okorie_Christian <okoriec01@gmail.com>
2023-04-09 12:41:44 +0000

    added verify now

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ import 'package:get/get.dart';
 import 'package:helloworld/view/welcome.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'view/main_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,6 +30,24 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.grey,
           ),
           home: const Welcome()),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          return const MainScreen();
+        } else {
+          return const Welcome();
+        }
+      },
     );
   }
 }
